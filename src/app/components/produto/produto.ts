855f9ade65ffbc803988b01511f83d5b6ca39bc4
Mayssa Gibson de Oliveira <mayssagibson@gmail.com>
2026-07-13 21:49:29 -0300

    Adiciona variaveis e classe do produto

--- src/app/components/produto/produto.ts
+++ src/app/components/produto/produto.ts
@@ -1,9 +1,20 @@
 import { Component } from '@angular/core';
-
+import { UpperCasePipe } from '@angular/common';
 @Component({
   selector: 'app-produto',
-  imports: [],
+  imports: [UpperCasePipe],
   templateUrl: './produto.html',
   styleUrl: './produto.css',
 })
-export class Produto {}
+// Adicionando variaveis e classe do produto
+export class Produto {
+  produto = 'Notebook Gamer';
+  preco = 5000;
+  mostrarProduto = true;
+  mostrarPreco = true;
+produtos =[
+    {nome: 'Teclado', preco: 49.99},
+    {nome: 'Mouse', preco: 29.99},
+    {nome: 'Monitor', preco: 229.99}
+  ];
+}
