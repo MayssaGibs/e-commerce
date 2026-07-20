5ae8b8086f1249350963f68e577961d5dce287d2
Mayssa Gibson de Oliveira <mayssagibson@gmail.com>
2026-07-20 18:55:39 -0300

    refactor: move componentes de produto para nova estrutura de pastas

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,7 +1,7 @@
 import { Component, signal } from '@angular/core';
 //import { RouterOutlet } from '@angular/router'; // remove a importação de RoterOutlet, pois não é necessário para este componente
 //import {Produto} from './components/produto/produto'; //importando a classe  produto do arquivo produto.ts para ser usado no componente app
-import { ListaProdutos } from './components/lista-produtos/lista-produtos';
+import { ListaProdutos } from './features/produtos/lista-produtos/lista-produtos';
 @Component({
   selector: 'app-root',
   imports: [ListaProdutos],
