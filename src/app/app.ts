037af8a97d41e74a08eb7bf79c47cdc28b450106
Mayssa Gibson de Oliveira <mayssagibson@gmail>
2026-07-21 19:48:27 -0300

    features(app) importa e cria o nome da loja

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,15 @@
 import { Component, signal } from '@angular/core';
-//import { RouterOutlet } from '@angular/router'; // remove a importação de RoterOutlet, pois não é necessário para este componente
-//import {Produto} from './components/produto/produto'; //importando a classe  produto do arquivo produto.ts para ser usado no componente app
-import { ListaProdutos } from './features/produtos/lista-produtos/lista-produtos';
+import { RouterOutlet, RouterLink } from '@angular/router'; //! remove a importação de RoterOutlet, pois não é necessário para este componente
+import { UpperCasePipe } from '@angular/common';
+//!import {Produto} from './components/produto/produto'; //importando a classe  produto do arquivo produto.ts para ser usado no componente app
+
 @Component({
   selector: 'app-root',
-  imports: [ListaProdutos],
+  imports: [RouterOutlet, RouterLink, UpperCasePipe],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('e-commerce');
+  nomeLoja = 'Loja das Tecs';
 }
