ae093600ca7a19a17894b9defcf0c610e965cc6c
Mayssa Gibson de Oliveira <mayssagibson@gmail.com>
2026-08-05 21:52:52 -0300

    feat(core) Adiciona MatCardModule para layout organizado

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,11 +2,15 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet, RouterLink } from '@angular/router';
 import { UpperCasePipe } from '@angular/common'; 
 import { usuarioLogado, login, logout } from './core/auth';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { Header } from './shared/layout/header/header';
+
 //!import {Produto} from './components/produto/produto'; //importando a classe  produto do arquivo produto.ts para ser usado no componente app
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink, UpperCasePipe],
+  imports: [RouterOutlet, RouterLink, UpperCasePipe, MatButtonModule ,MatCardModule, Header],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
