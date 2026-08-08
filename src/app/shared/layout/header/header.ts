b846f558aece35953b39c260fe2924d7bcaf70d3
Mayssa Gibson de Oliveira <mayssagibson@gmail.com>
2026-08-07 21:31:29 -0300

    feat(carrinho): cria serviço de gerenciamento do carrinho com Signals

--- src/app/shared/layout/header/header.ts
+++ src/app/shared/layout/header/header.ts
@@ -2,13 +2,17 @@ import { Component } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar'
 import { MatButtonModule } from '@angular/material/button';
 import { RouterLink } from '@angular/router';
+import { CarrinhoService } from '../../../core/services/carrinho.service';
+import { inject } from '@angular/core';
 
 @Component({
   selector: 'app-header',
-  imports: [MatToolbarModule,MatButtonModule, RouterLink],
+  imports: [MatToolbarModule,MatButtonModule, RouterLink,],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
 export class Header {
   nomeLoja = 'Mercado das Tecs';
+  private carrinhoService = inject(CarrinhoService);
+  quantidadeHeader = this.carrinhoService.quantidadeItens;
 }
