738b56d6dee6956c07d85db1ca6cba3973a391bf
Mayssa Gibson de Oliveira <mayssagibson@gmail.com>
2026-08-12 21:34:08 -0300

    feat(carrinho): adiciona computed properties para quantidade, total e status do carrinho

--- src/app/core/services/carrinho.service.ts
+++ src/app/core/services/carrinho.service.ts
@@ -1,5 +1,9 @@
 import { Injectable, signal, computed } from "@angular/core";
 
+type ItemCarrinho ={
+    nome: string;
+    preco: number;
+}
 @Injectable({
     providedIn: 'root'
 })
@@ -7,13 +11,13 @@ import { Injectable, signal, computed } from "@angular/core";
 export class CarrinhoService {
 
 //! Estado Global
-private carrinho = signal<{nome: string; preco: number}[]>([]);
+private carrinho = signal<ItemCarrinho[]>([]);
 
 //? Seleções
 itens = computed(() => this.carrinho());
 quantidadeItens = computed(() => this.carrinho().length);
-totalItens = computed(() => 
-this.carrinho().reduce((total, item) => total + item.preco,0)); 
+totalItens = computed(() =>  this.carrinho().reduce((total, item) => total + item.preco,0)); 
+carrinhoVazio = computed(() => this.carrinho().length === 0);
 
  // TODO: Ação Adicionar Produtos
 adicionar(produto:{nome: string; preco: number}){
