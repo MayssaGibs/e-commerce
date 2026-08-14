823d28a693d30153d733e2797d982fde29994bbb
Mayssa Gibson de Oliveira <mayssagibson@gmail.com>
2026-08-13 21:16:02 -0300

    refactor(app): simplificar imports do componente raiz

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,7 +1,6 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet, RouterLink } from '@angular/router';
 import { UpperCasePipe } from '@angular/common'; 
-import { usuarioLogado, login, logout } from './core/auth';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { Header } from './shared/layout/header/header';
@@ -10,14 +9,10 @@ import { Header } from './shared/layout/header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink, UpperCasePipe, MatButtonModule ,MatCardModule, Header],
+  imports: [RouterOutlet, MatButtonModule ,MatCardModule, Header],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('e-commerce');
-  nomeLoja = 'Loja das Tecs';
-  usuarioLogado = usuarioLogado;
-  login = login;
-  logout = logout;
 }
