1e8fb505ba6c317b73eeaffbe11f1ac583be955e
Mayssa Gibson de Oliveira <mayssagibson@gmail.com>
2026-08-14 21:15:22 -0300

    feat(security): trata erro 403 com redirecionamento

--- src/app/core/interceptors/http.interceptor.ts
+++ src/app/core/interceptors/http.interceptor.ts
@@ -4,13 +4,16 @@ import { catchError } from "rxjs";
 import { throwError } from "rxjs";
 import { inject } from "@angular/core";
 import { AuthService } from "../services/auth.service";
+import { Router } from "@angular/router";
 
 export const httpInterceptor: HttpInterceptorFn =  (req, next) => {
 
     console.log('Interceptor Requisição: ', req.url);
 
     const authService = inject(AuthService);
+    const router = inject(Router);
     const token = authService.obterToken();
+
 
     const novaReq = token ?
     req.clone({
@@ -18,7 +21,7 @@ export const httpInterceptor: HttpInterceptorFn =  (req, next) => {
             Authorization: `Bearer ${token}`,
         },
     }):req;
-   return next (novaReq).pipe(
+return next (novaReq).pipe(
     tap({
         next: (event) => {console.log('Responde;', event)},
         error: (error) => {console.error('Erro na requisição; ', error)}
@@ -28,12 +31,23 @@ export const httpInterceptor: HttpInterceptorFn =  (req, next) => {
     console.error('Erro de Requisição Global: ', error);
 
     if (error.status === 401){
+
         console.error('Erro de autenticação de Usuário:', error);
+        authService.logout();
+        router.navigateByUrl('/login');
+
     }
     if (error.status === 500){
-        console.warn('Erro interno do servidor !', error);
+
+        console.warn('Erro interno do servidor!', error);
     }
-    return throwError(() => error);
+    
+    if(error.status === 403){
+        console.warn('Acesso proibido, Usuário sem permissão.');
+        router.navigateByUrl('/produtos');
+    }
+
+return throwError(() => error);
     }),
    );
 };
